dd0e56571b10347d1a65eac90f94ef72c4dd42e5
Wenjuan Li <wenjuan.li@compass.com>
2022-07-06 11:11:59 +0800

    Initial Commit

---
+++ CounterApp/CounterAppApp.swift
@@ -0,0 +1,17 @@
+//
+//  CounterAppApp.swift
+//  CounterApp
+//
+//  Created by Wenjuan Li on 2022/7/6.
+//
+
+import SwiftUI
+
+@main
+struct CounterAppApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
