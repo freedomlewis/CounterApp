e0b2bec8b9360d80210f5cd253a8d52451cd84b5
Wenjuan Li <wenjuan.li@compass.com>
2022-07-08 10:23:12 +0800

    fix test

--- CounterAppTests/CounterReducerTests.swift
+++ CounterAppTests/CounterReducerTests.swift
@@ -12,13 +12,40 @@ import XCTest
 class CounterReducerTests: XCTestCase {
 
     let state = CounterState()
+    let queue = DispatchQueue.test
 
     func testCountUpAndDown() throws {
-        let store = TestStore(initialState: state, reducer: counterReducer, environment: CounterEnviroment())
-        store.send(.onIncBtnTapped) {
+        let store = TestStore(
+            initialState: state,
+            reducer: counterReducer,
+            environment: CounterEnviroment(
+                queue: queue.eraseToAnyScheduler(),
+                increment: {value, max in
+                    if value >= 1 {
+                        return Effect(error: ServiceError(msg: "Inc failed"))
+                    }else {
+                        return Effect(value: value + 1)
+                    }
+                },
+                decrement: {value, min in Effect(value: value - 1)})
+        )
+        
+        store.send(.onIncBtnTapped)
+        queue.advance()
+        store.receive(.counterResponse(.success(1))) {
             $0.count = 1
         }
-        store.send(.onDecBtnTapped){
+        
+        store.send(.onIncBtnTapped)
+        queue.advance()
+        store.receive(.counterResponse(.failure(ServiceError(msg: "Inc failed")))) {
+            $0.count = 1
+            $0.alertMsg = "Inc failed"
+        }
+        
+        store.send(.onDecBtnTapped)
+        queue.advance()
+        store.receive(.counterResponse(.success(0))) {
             $0.count = 0
         }
     }
